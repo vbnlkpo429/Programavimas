7a95499f198d3a357852fd72b3eac3e8eff62624
Simas <simas.zygas@gmail.com>
2023-05-19 14:45:13 +0300

    Failas

---
+++ 10paskaita/funkcijadebug.py
@@ -0,0 +1,12 @@
+def patikrinti(sk):
+    if sk < 0:
+        print("Skaicius mazesnis uz 0")
+    if sk == 0:
+        print("Skaicius yra 0")
+    if sk > 0:
+        print("Skaicius daugiau uz 0")
+
+patikrinti(6)
+patikrinti(12)
+patikrinti(15)
+patikrinti(17)
